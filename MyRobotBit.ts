82bdc89f2801dd66de055ffdb5203e859b2e37dc
myrobotbit <41653060+myrobotbit@users.noreply.github.com>
2018-10-16 18:04:43 +0700

    Add files via upload

--- MyRobotBit.ts
+++ MyRobotBit.ts
@@ -1,88 +1,87 @@
-﻿    enum Motors {
-        //% blockId=Motor_motor_A
+﻿/**
+* Coding control  Motor A and Motor B
+*/
+ enum Motors {
         //% block="motor A"
         MotorA,
-        //% blockId=Motor_motor_B
         //% block="motor B"
         MotorB,
-        //% blockId=Motor_motor_AB
         //% block="motor AB"
         MotorAB
     }
 
-    enum MotorDirection {
+enum Direction {
         //% block="forward"
         Forward,
         //% block="reverse"
         Reverse
     }
 
-    enum StopMode {
+enum StopMode {
         //% block="brake"
         Brake,
         //% block="coast"
         Coast
     }
 
-    enum Rotated {
+enum Rotated {
         //% block="left"
         Left,
         //% block="right"
         Right
     }
 
-    enum RotatedmS {
+enum RotatedmS {
         //% block="left"
         Left,
         //% block="right"
         Right
     }
 
-    enum Turn {
+enum Turn {
         //% block="left"
         Left,
         //% block="right"
         Right
     }
 
-//% weight=50 color="#ff6600" weight=10 icon="\uf11e"
+//% weight=50 color=#ff6600 icon="\uf11e"
 namespace MyRobotBit {
 
      /**	
-     * Turns on motor, forward, reverse at the requested speed 
-     *
-     * @param speed   motor speed 0-100, eg:50
+     * Turns on motor forward reverse at the requested speed 
+     * @param speed   motor speed 0-100, eg: 50
      */
-    //% blockId="MyRobotBit_motorON" block="motor %Motors | direction %MotorDirection | speed %speed"
+    //% blockId="MyRobotBit_motorON" block="motor %Motors | direction %Direction | speed %speed"
     //% speed.min=0 speed.max=100
-    export function motorOn(motorSelect: Motors, dir: MotorDirection, speed: number): void {
+    export function motorOn(motorSelect:Motors, motorDir:Direction, speed:number): void {
         let motorspeed = pins.map(speed,0,100,0,1023)     
  
-	if (motorSelect==Motors.MotorAB && dir==MotorDirection.Forward) {
+	if (motorSelect==Motors.MotorAB && motorDir==Direction.Forward) {
                         pins.analogWritePin(AnalogPin.P13, motorspeed)
                         pins.digitalWritePin(DigitalPin.P14, 0)
 			pins.analogWritePin(AnalogPin.P15, motorspeed)
                         pins.digitalWritePin(DigitalPin.P16, 0)
 	}
-	else if (motorSelect==Motors.MotorA && dir==MotorDirection.Forward) {
+	else if (motorSelect==Motors.MotorA && motorDir==Direction.Forward) {
                         pins.analogWritePin(AnalogPin.P13, motorspeed)
                         pins.digitalWritePin(DigitalPin.P14, 0)
 	}
-	else if (motorSelect==Motors.MotorB && dir==MotorDirection.Forward) {
+	else if (motorSelect==Motors.MotorB && motorDir==Direction.Forward) {
                         pins.analogWritePin(AnalogPin.P15, motorspeed)
                         pins.digitalWritePin(DigitalPin.P16, 0)
 	}
-	if (motor==MotorsSelect.MotorAB && dir==MotorDirection.Reverse) {
+	if (motor==MotorsSelect.MotorAB && motorDir==Direction.Reverse) {
                         pins.analogWritePin(AnalogPin.P14, motorspeed)
                         pins.digitalWritePin(DigitalPin.P13, 0)
                         pins.analogWritePin(AnalogPin.P16, motorspeed)
                         pins.digitalWritePin(DigitalPin.P15, 0)
 	}
-	else if (motorSelect==Motors.MotorA && dir==MotorDirection.Reverse) {
+	else if (motorSelect==Motors.MotorA && motorDir==Direction.Reverse) {
                         pins.analogWritePin(AnalogPin.P14, motorspeed)
                         pins.digitalWritePin(DigitalPin.P13, 0)
 	}
-	else if (motorSelect==Motors.MotorB && dir==MotorDirection.Reverse) {
+	else if (motorSelect==Motors.MotorB && motorDir==Direction.Reverse) {
                         pins.analogWritePin(AnalogPin.P16, motorspeed)
                         pins.digitalWritePin(DigitalPin.P15, 0)
 	}
