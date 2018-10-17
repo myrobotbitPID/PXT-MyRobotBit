7bf152f6d5144d46da9d68b5faa0a12f4ea243a6
myrobotbitPID <42580526+myrobotbitPID@users.noreply.github.com>
2018-10-17 11:28:46 +0700

    Update MyRobotBit.ts

--- MyRobotBit.ts
+++ MyRobotBit.ts
@@ -44,4 +44,27 @@ namespace MyRobotBit {
              pins.digitalWritePin(DigitalPin.P15, 0)
         }
     }
+
+      /**MotorON Block to drives motor forward and backward. The speed motor is adjustable between 0 to 100.
+      * @param speed percent of maximum speed, eg: 50
+      */
+    //% blockId="Mybit_MotorON" block="motor %motorSEL|speed %Speed"
+    //% Speed.min=0 Speed.max=100
+    //% weight=95
+    export function MotorON(Motor: motorSEL, Speed: number): void {  
+        let motorspeed = pins.map(speed,0,100,0,1023)     
+        if (Motor == motorSEL.MotorA) {
+             pins.analogWritePin(AnalogPin.P13, motorspeed)
+             pins.digitalWritePin(DigitalPin.P14, 0)
+	     pins.analogWritePin(AnalogPin.P15, motorspeed)
+             pins.digitalWritePin(DigitalPin.P16, 0)
+        }
+        if (Motor == motorSEL.MotorB) {
+            pins.analogWritePin(AnalogPin.P14, motorspeed)
+             pins.digitalWritePin(DigitalPin.P13, 0)
+             pins.analogWritePin(AnalogPin.P16, motorspeed)
+             pins.digitalWritePin(DigitalPin.P15, 0)
+        }
+    }
+
 }
